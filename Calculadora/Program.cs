dd881b816ee902e4829278a754d00552fbb8d61a
Diego Molina <diegomolina_30@hotmail.com>
2023-05-09 17:02:27 -0500

    first commit

---
+++ Calculadora/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Programa
+{
+    internal class Program
+    {   
+
+        public double Sumar()
+        {
+            return 1.0;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
