25bf544414ba5e33830546ec92f43410d34553c5
Diego Molina <diegomolina_30@hotmail.com>
2023-05-10 13:59:51 -0500

    add folder src with new files

---
+++ Calculadora/src/Main.cs
@@ -0,0 +1,20 @@
+﻿using Calculadora.src.view;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static Calculadora.src.view.Consola;
+
+namespace Calculadora.src
+{
+    internal class main
+    {
+        static void Main(string[] args)
+        {
+            Consola console = new Consola();
+            console.Menu(); //Se inicia el menu
+        }
+    }
+}
---
+++ Calculadora/src/view/Consola.cs
@@ -0,0 +1,40 @@
+﻿using Calculadora.src.controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.src.view
+{
+    internal class Consola
+    {
+        public void Menu()
+        {
+            Console.WriteLine(
+                "1. Suma\n" +
+                "2. Resta\n" +
+                "3. Multiplicación\n" +
+                "4. División\n" +
+                "5. Operaciones guardadas\n" +
+                "6. Salir");
+
+            try {
+                int opcion = int.Parse(Console.ReadLine());
+                ConsolaController controller = new ConsolaController();
+                controller.controllerConsola(opcion);
+
+            }
+            catch (Exception ex) {
+                Console.WriteLine("Error !", ex);
+            }
+            
+        }
+
+        public int valor()
+        {
+
+            return 1550;
+        }
+    }
+}
